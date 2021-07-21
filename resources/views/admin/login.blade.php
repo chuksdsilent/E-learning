ff18e4394687f15cb741d4daa5c90f71fa2167ac
Chuks <chuksdsilent@gmail.com>
2021-07-21 14:15:15 +0100

    initial Commit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -19,7 +19,7 @@
     <div class="login">
         <div class="navbar navbar-expand-lg navbar-light">
             <nav class="container">
-                <a class="navbar-brand" href="#">Acadazone</a>
+                <a class="navbar-brand" href="#">E-Learning</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
